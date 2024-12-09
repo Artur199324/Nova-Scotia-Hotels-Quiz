819011cdb0b1f95c5dde427be93549af161882f1
Artur199324 <artyr241993@gmail.com>
2024-12-09 16:35:06 +0200

    Initial Commit

---
+++ Nova Scotia Hotels Quiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nova Scotia Hotels Quiz
+//
+//  Created by Artur on 09.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nova Scotia Hotels Quiz/Nova_Scotia_Hotels_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nova_Scotia_Hotels_QuizApp.swift
+//  Nova Scotia Hotels Quiz
+//
+//  Created by Artur on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Nova_Scotia_Hotels_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
